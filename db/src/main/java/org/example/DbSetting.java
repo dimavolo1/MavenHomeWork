89890f612c28d16f7b1b1fc4e63216160a473052
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-07-11 13:19:51 +0500

    homeWork

---
+++ db/src/main/java/org/example/DbSetting.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class DbSetting {
+
+    String name;
+    String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
